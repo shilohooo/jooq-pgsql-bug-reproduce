0ff05e2f78549d95b8b44e52ca1853914f17d3f2
shiloh <lixiaolei595@gmail.com>
2025-12-09 17:35:45 +0800

    refactor: use in-memory compilation for code generation

--- src/main/java/com/github/shiloh/jooq/codegen/CustomJooqCodeGeneratorStrategy.java
+++ src/main/java/com/github/shiloh/jooq/codegen/CustomJooqCodeGeneratorStrategy.java
@@ -1,37 +1,37 @@
-package com.github.shiloh.jooq.codegen;
-
-import com.github.shiloh.base.dao.impl.BaseDaoImpl;
-import lombok.extern.slf4j.Slf4j;
-import org.jooq.codegen.DefaultGeneratorStrategy;
-import org.jooq.meta.Definition;
-import org.jooq.meta.TableDefinition;
-
-/**
- * 自定义 JOOQ 代码生成策略
- *
- * @author shiloh
- */
-@Slf4j
-public class CustomJooqCodeGeneratorStrategy extends DefaultGeneratorStrategy {
-    @Override
-    public String getJavaClassExtends(Definition definition, Mode mode) {
-        log.info("com.github.shiloh.jooq.codegen.CustomJooqGenerator.getJavaClassExtends: {}, {}", definition, mode);
-        if (definition instanceof TableDefinition && Mode.DAO.equals(mode)) {
-            return BaseDaoImpl.class.getName();
-        }
-
-        return super.getJavaClassExtends(definition, mode);
-    }
-
-    @Override
-    public String getJavaClassName(Definition definition, Mode mode) {
-        final String javaClassName = super.getJavaClassName(definition, mode);
-        log.info("com.github.shiloh.jooq.codegen.CustomJooqGenerator.getJavaClassName: {}, {}, {}", definition.getClass().getName(), mode, javaClassName);
-        final String javaClassNameWithoutPrefix = javaClassName.replace("T", "");
-        return switch (mode) {
-            case DEFAULT -> String.format("%sTable", javaClassNameWithoutPrefix);
-            case POJO -> String.format("%sPojo", javaClassNameWithoutPrefix);
-            default -> javaClassNameWithoutPrefix;
-        };
-    }
-}
+// package com.github.shiloh.jooq.codegen;
+//
+// import com.github.shiloh.base.dao.impl.BaseDaoImpl;
+// import lombok.extern.slf4j.Slf4j;
+// import org.jooq.codegen.DefaultGeneratorStrategy;
+// import org.jooq.meta.Definition;
+// import org.jooq.meta.TableDefinition;
+//
+// /**
+//  * 自定义 JOOQ 代码生成策略
+//  *
+//  * @author shiloh
+//  */
+// @Slf4j
+// public class CustomJooqCodeGeneratorStrategy extends DefaultGeneratorStrategy {
+//     @Override
+//     public String getJavaClassExtends(Definition definition, Mode mode) {
+//         log.info("com.github.shiloh.jooq.codegen.CustomJooqGenerator.getJavaClassExtends: {}, {}", definition, mode);
+//         if (definition instanceof TableDefinition && Mode.DAO.equals(mode)) {
+//             return BaseDaoImpl.class.getName();
+//         }
+//
+//         return super.getJavaClassExtends(definition, mode);
+//     }
+//
+//     @Override
+//     public String getJavaClassName(Definition definition, Mode mode) {
+//         final String javaClassName = super.getJavaClassName(definition, mode);
+//         log.info("com.github.shiloh.jooq.codegen.CustomJooqGenerator.getJavaClassName: {}, {}, {}", definition.getClass().getName(), mode, javaClassName);
+//         final String javaClassNameWithoutPrefix = javaClassName.replace("T", "");
+//         return switch (mode) {
+//             case DEFAULT -> String.format("%sTable", javaClassNameWithoutPrefix);
+//             case POJO -> String.format("%sPojo", javaClassNameWithoutPrefix);
+//             default -> javaClassNameWithoutPrefix;
+//         };
+//     }
+// }
